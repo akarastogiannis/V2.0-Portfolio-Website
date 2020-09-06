ac6b35dc799e6153d775b29bc54581ba640f835f
akarastogiannis <akarastogiannis@outlook.com>
2020-09-06 00:57:49 -0400

    Major Revisions and Changes, but subtle overall design change.

---
+++ javascripts/portfolio.js
@@ -0,0 +1,6 @@
+
+
+// Link not Done Function to warn the user when they click on a link that is not working
+function linkNotDone() {
+  alert("Sorry, This Link is currently under Construction");
+};
---
+++ javascripts/universal.js
@@ -0,0 +1,4 @@
+
+// Code to get the new Date everytime for Copyright Laws
+var year = new Date().getFullYear();
+document.getElementById("copyrightYear").innerHTML = year;
